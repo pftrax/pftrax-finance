dabb85288cbe6ad10a75ad4f602e05f113a06a45
PFTrax <94227482+pftrax@users.noreply.github.com>
2022-01-27 21:40:00 +0800

    Add files via upload

---
+++ src/components/SearchModal/CurrencySearch.tsx
@@ -0,0 +1,179 @@
+import React, { KeyboardEvent, RefObject, useCallback, useMemo, useRef, useState, useEffect } from 'react'
+import { Currency, ETHER, Token } from '@rimauswap-sdk/sdk'
+import { Text, Input, Box, Svg } from '@rimauswap-libs/uikit'
+import { useTranslation } from 'contexts/Localization'
+import { FixedSizeList } from 'react-window'
+import { useAudioModeManager } from 'state/user/hooks'
+import useDebounce from 'hooks/useDebounce'
+import useActiveWeb3React from 'hooks/useActiveWeb3React'
+import { useAllTokens, useToken, useIsUserAddedToken, useFoundOnInactiveList } from '../../hooks/Tokens'
+import { isAddress } from '../../utils'
+import Column, { AutoColumn } from '../Layout/Column'
+import Row from '../Layout/Row'
+import CommonBases from './CommonBases'
+import CurrencyList from './CurrencyList'
+import { filterTokens, useSortedTokensByQuery } from './filtering'
+import useTokenComparator from './sorting'
+
+import ImportRow from './ImportRow'
+
+interface CurrencySearchProps {
+  selectedCurrency?: Currency | null
+  onCurrencySelect: (currency: Currency) => void
+  otherSelectedCurrency?: Currency | null
+  showCommonBases?: boolean
+  showImportView: () => void
+  setImportToken: (token: Token) => void
+}
+
+const swapSound = new Audio('swap.mp3')
+
+function CurrencySearch({
+  selectedCurrency,
+  onCurrencySelect,
+  otherSelectedCurrency,
+  showCommonBases,
+  showImportView,
+  setImportToken,
+}: CurrencySearchProps) {
+  const { t } = useTranslation()
+  const { chainId } = useActiveWeb3React()
+
+  // refs for fixed size lists
+  const fixedList = useRef<FixedSizeList>()
+
+  const [searchQuery, setSearchQuery] = useState<string>('')
+  const debouncedQuery = useDebounce(searchQuery, 200)
+
+  const [invertSearchOrder] = useState<boolean>(false)
+
+  const allTokens = useAllTokens()
+
+  // if they input an address, use it
+  const searchToken = useToken(debouncedQuery)
+  const searchTokenIsAdded = useIsUserAddedToken(searchToken)
+
+  const [audioPlay] = useAudioModeManager()
+
+  const showETH: boolean = useMemo(() => {
+    const s = debouncedQuery.toLowerCase().trim()
+    return s === '' || s === 'b' || s === 'bn' || s === 'bnb'
+  }, [debouncedQuery])
+
+  const tokenComparator = useTokenComparator(invertSearchOrder)
+
+  const filteredTokens: Token[] = useMemo(() => {
+    return filterTokens(Object.values(allTokens), debouncedQuery)
+  }, [allTokens, debouncedQuery])
+
+  const sortedTokens: Token[] = useMemo(() => {
+    return filteredTokens.sort(tokenComparator)
+  }, [filteredTokens, tokenComparator])
+
+  const filteredSortedTokens = useSortedTokensByQuery(sortedTokens, debouncedQuery)
+
+  const handleCurrencySelect = useCallback(
+    (currency: Currency) => {
+      onCurrencySelect(currency)
+      if (audioPlay) {
+        swapSound.play()
+      }
+    },
+    [audioPlay, onCurrencySelect],
+  )
+
+  // manage focus on modal show
+  const inputRef = useRef<HTMLInputElement>()
+
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
+
+  const handleInput = useCallback((event) => {
+    const input = event.target.value
+    const checksummedInput = isAddress(input)
+    setSearchQuery(checksummedInput || input)
+    fixedList.current?.scrollTo(0)
+  }, [])
+
+  const handleEnter = useCallback(
+    (e: KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter') {
+        const s = debouncedQuery.toLowerCase().trim()
+        if (s === 'bnb') {
+          handleCurrencySelect(ETHER)
+        } else if (filteredSortedTokens.length > 0) {
+          if (
+            filteredSortedTokens[0].symbol?.toLowerCase() === debouncedQuery.trim().toLowerCase() ||
+            filteredSortedTokens.length === 1
+          ) {
+            handleCurrencySelect(filteredSortedTokens[0])
+          }
+        }
+      }
+    },
+    [filteredSortedTokens, handleCurrencySelect, debouncedQuery],
+  )
+
+  // if no results on main list, show option to expand into inactive
+  const inactiveTokens = useFoundOnInactiveList(debouncedQuery)
+  const filteredInactiveTokens: Token[] = useSortedTokensByQuery(inactiveTokens, debouncedQuery)
+
+  return (
+    <>
+      <div>
+        <AutoColumn gap="16px">
+          <Row style={{position:'relative'}}>
+            <Svg style={{position:'absolute', top:15, left:10, opacity:0.5}} viewBox="0 0 24 24">
+              <path d="M15.5 13.9996H14.71L14.43 13.7296C15.63 12.3296 16.25 10.4196 15.91 8.38965C15.44 5.60965 13.12 3.38965 10.32 3.04965C6.09001 2.52965 2.53002 6.08965 3.05002 10.3196C3.39002 13.1196 5.61002 15.4396 8.39002 15.9096C10.42 16.2496 12.33 15.6296 13.73 14.4296L14 14.7096V15.4996L18.25 19.7496C18.66 20.1596 19.33 20.1596 19.74 19.7496C20.15 19.3396 20.15 18.6696 19.74 18.2596L15.5 13.9996ZM9.50002 13.9996C7.01002 13.9996 5.00002 11.9896 5.00002 9.49965C5.00002 7.00965 7.01002 4.99965 9.50002 4.99965C11.99 4.99965 14 7.00965 14 9.49965C14 11.9896 11.99 13.9996 9.50002 13.9996Z" />
+            </Svg>
+            <Input
+              id="token-search-input"
+              placeholder={t('Search name or paste address')}
+              scale="lg"
+              autoComplete="off"
+              value={searchQuery}
+              style={{paddingLeft:35}}
+              ref={inputRef as RefObject<HTMLInputElement>}
+              onChange={handleInput}
+              onKeyDown={handleEnter}
+            />
+          </Row>
+          {showCommonBases && (
+            <CommonBases chainId={chainId} onSelect={handleCurrencySelect} selectedCurrency={selectedCurrency} />
+          )}
+        </AutoColumn>
+        {searchToken && !searchTokenIsAdded ? (
+          <Column style={{ padding: '20px 0', height: '100%' }}>
+            <ImportRow token={searchToken} showImportView={showImportView} setImportToken={setImportToken} />
+          </Column>
+        ) : filteredSortedTokens?.length > 0 || filteredInactiveTokens?.length > 0 ? (
+          <Box margin="24px -24px">
+            <CurrencyList
+              height={390}
+              showETH={showETH}
+              currencies={
+                filteredInactiveTokens ? filteredSortedTokens.concat(filteredInactiveTokens) : filteredSortedTokens
+              }
+              breakIndex={inactiveTokens && filteredSortedTokens ? filteredSortedTokens.length : undefined}
+              onCurrencySelect={handleCurrencySelect}
+              otherCurrency={otherSelectedCurrency}
+              selectedCurrency={selectedCurrency}
+              fixedListRef={fixedList}
+              showImportView={showImportView}
+              setImportToken={setImportToken}
+            />
+          </Box>
+        ) : (
+          <Column style={{ padding: '20px', height: '100%' }}>
+            <Text color="textSubtle" textAlign="center" mb="20px">
+              {t('No results found.')}
+            </Text>
+          </Column>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default CurrencySearch
