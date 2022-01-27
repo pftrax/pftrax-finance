1cd43ce637a8029133ced9c2442496ca3b251c56
PFTrax <94227482+pftrax@users.noreply.github.com>
2022-01-27 21:01:15 +0800

    Add files via upload

---
+++ src/views/Predictions/components/FlexRow.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { Flex } from '@rimauswap-libs/uikit'
+
+const FlexRow = styled(Flex).attrs({ alignItems: 'center' })`
+  flex: 1;
+`
+
+export default FlexRow
