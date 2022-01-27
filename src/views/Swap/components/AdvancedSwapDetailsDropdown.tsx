c958b2c2cf403a1faabbd5fe7a87615b33303bc9
PFTrax <94227482+pftrax@users.noreply.github.com>
2022-01-27 21:07:12 +0800

    Add files via upload

---
+++ src/views/Swap/components/AdvancedSwapDetailsDropdown.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import useLastTruthy from 'hooks/useLast'
+import { AdvancedSwapDetails, AdvancedSwapDetailsProps } from './AdvancedSwapDetails'
+
+const AdvancedDetailsFooter = styled.div<{ show: boolean }>`
+  margin-top: ${({ show }) => (show ? '16px' : '-16px')};
+  padding-top: 16px;
+  padding-bottom: 16px;
+  width: 100%;
+  max-width: 400px;
+  border-radius: 20px;
+  background-color: ${({ theme }) => theme.colors.invertedContrast};
+  transform: ${({ show }) => (show ? 'translateY(0%)' : 'translateY(-100%)')};
+  display: ${({ show }) => (show ? 'block' : 'none')};
+  transition: all 300ms ease-in-out;
+`
+
+export default function AdvancedSwapDetailsDropdown({ trade, ...rest }: AdvancedSwapDetailsProps) {
+  const lastTrade = useLastTruthy(trade)
+
+  return (
+    <AdvancedDetailsFooter show={Boolean(trade)}>
+      <AdvancedSwapDetails {...rest} trade={trade ?? lastTrade ?? undefined} />
+    </AdvancedDetailsFooter>
+  )
+}
---
+++ src/views/Swap/components/ConfirmSwapModal.tsx
@@ -0,0 +1,125 @@
+import React, { useCallback, useMemo } from 'react'
+import { currencyEquals, Trade } from '@rimauswap-sdk/sdk'
+import { InjectedModalProps } from '@rimauswap-libs/uikit'
+import Tap from "@tapfiliate/tapfiliate-js";
+import { useTranslation } from 'contexts/Localization'
+import TransactionConfirmationModal, {
+  ConfirmationModalContent,
+  TransactionErrorContent,
+} from 'components/TransactionConfirmationModal'
+import useActiveWeb3React from '../../../hooks/useActiveWeb3React'
+import SwapModalFooter from './SwapModalFooter'
+import SwapModalHeader from './SwapModalHeader'
+
+/**
+ * Returns true if the trade requires a confirmation of details before we can submit it
+ * @param tradeA trade A
+ * @param tradeB trade B
+ */
+function tradeMeaningfullyDiffers(tradeA: Trade, tradeB: Trade): boolean {
+  return (
+    tradeA.tradeType !== tradeB.tradeType ||
+    !currencyEquals(tradeA.inputAmount.currency, tradeB.inputAmount.currency) ||
+    !tradeA.inputAmount.equalTo(tradeB.inputAmount) ||
+    !currencyEquals(tradeA.outputAmount.currency, tradeB.outputAmount.currency) ||
+    !tradeA.outputAmount.equalTo(tradeB.outputAmount)
+  )
+}
+interface ConfirmSwapModalProps {
+  trade?: Trade
+  originalTrade?: Trade
+  attemptingTxn: boolean
+  txHash?: string
+  recipient: string | null
+  allowedSlippage: number
+  onAcceptChanges: () => void
+  onConfirm: () => void
+  swapErrorMessage?: string
+  customOnDismiss?: () => void
+}
+
+const ConfirmSwapModal: React.FC<InjectedModalProps & ConfirmSwapModalProps> = ({
+  trade,
+  originalTrade,
+  onAcceptChanges,
+  allowedSlippage,
+  onConfirm,
+  onDismiss,
+  customOnDismiss,
+  recipient,
+  swapErrorMessage,
+  attemptingTxn,
+  txHash,
+}) => {
+  const showAcceptChanges = useMemo(
+    () => Boolean(trade && originalTrade && tradeMeaningfullyDiffers(trade, originalTrade)),
+    [originalTrade, trade],
+  )
+
+  const { t } = useTranslation()
+  const { account } = useActiveWeb3React();
+  
+  React.useEffect(() => {
+    if(account && !attemptingTxn && txHash){
+      Tap.conversion(txHash, '1', { customer_id: account });
+    }
+  }, [account, attemptingTxn, txHash]);
+
+
+  const modalHeader = useCallback(() => {
+    return trade ? (
+      <SwapModalHeader
+        trade={trade}
+        allowedSlippage={allowedSlippage}
+        recipient={recipient}
+        showAcceptChanges={showAcceptChanges}
+        onAcceptChanges={onAcceptChanges}
+      />
+    ) : null
+  }, [allowedSlippage, onAcceptChanges, recipient, showAcceptChanges, trade])
+
+  const modalBottom = useCallback(() => {
+    return trade ? (
+      <SwapModalFooter
+        onConfirm={onConfirm}
+        trade={trade}
+        disabledConfirm={showAcceptChanges}
+        swapErrorMessage={swapErrorMessage}
+        allowedSlippage={allowedSlippage}
+      />
+    ) : null
+  }, [allowedSlippage, onConfirm, showAcceptChanges, swapErrorMessage, trade])
+
+  // text to show while loading
+  const pendingText = t('Swapping %amountA% %symbolA% for %amountB% %symbolB%', {
+    amountA: trade?.inputAmount?.toSignificant(6) ?? '',
+    symbolA: trade?.inputAmount?.currency?.symbol ?? '',
+    amountB: trade?.outputAmount?.toSignificant(6) ?? '',
+    symbolB: trade?.outputAmount?.currency?.symbol ?? '',
+  })
+
+  const confirmationContent = useCallback(
+    () =>
+      swapErrorMessage ? (
+        <TransactionErrorContent onDismiss={onDismiss} message={swapErrorMessage} />
+      ) : (
+        <ConfirmationModalContent topContent={modalHeader} bottomContent={modalBottom} />
+      ),
+    [onDismiss, modalBottom, modalHeader, swapErrorMessage],
+  )
+
+  return (
+    <TransactionConfirmationModal
+      title={t('Confirm Swap')}
+      onDismiss={onDismiss}
+      customOnDismiss={customOnDismiss}
+      attemptingTxn={attemptingTxn}
+      hash={txHash}
+      content={confirmationContent}
+      pendingText={pendingText}
+      currencyToAdd={trade?.outputAmount.currency}
+    />
+  )
+}
+
+export default ConfirmSwapModal
